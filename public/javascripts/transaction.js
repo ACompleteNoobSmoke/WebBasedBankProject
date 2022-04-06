4af7437015cdc0e7b95b072adbf8ad096fa4af2d
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-04-06 07:15:14 -0500

    Worked on the first version of project

--- public/javascripts/transaction.js
+++ public/javascripts/transaction.js
@@ -6,49 +6,39 @@ if(document.readyState === 'loading'){
 
 function ready(){
     let accountType = document.getElementById("accountType");
-    //let amountElement = document.getElementsByName("transactionAmount")[0];
-    accountChange(accountType);
-    //amountMinMax(amountElement);
+    let pageHeaders = document.getElementsByClassName('page-header')[0];
+    let pageheaderArray = ['PROFILE', 'DEPOSIT', 'WITHDRAW', 'TRANSFER', 'EDIT PROFILE'];
+    accountChange(pageHeaders, accountType);
+    modifyHeaderNav(pageHeaders, pageheaderArray);
+}
+
+function modifyHeaderNav(pageHeaders, pageheaderArray){
+    let headerLinks = document.getElementsByClassName('header-links')[0];
+    if(pageheaderArray.includes(pageHeaders.innerHTML)){
+        console.log(pageHeaders.innerHTML);
+        headerLinks.style.visibility = "hidden";
+    }else{
+        headerLinks.style.visibility = "visible";
+    }
 }
 
 
-
-function accountChange(accountType){
-    let currentAmountChecking = document.getElementById("current-amount-checking");
-    let currentAmountSaving = document.getElementById("current-amount-saving");
-    console.log(currentAmountChecking);
-    console.log(currentAmountSaving)
-    currentAmountSaving.style.display = "none";
-    accountType.addEventListener("change", e => {
-        if(e.target.value == 'Saving'){
-            currentAmountChecking.style.display = "none";
-            currentAmountSaving.style.display = "block";
-        }else{
-            currentAmountChecking.style.display = "block";
-            currentAmountSaving.style.display = "none";
-        }
-    })
+function accountChange(pageHeaders, accountType){
+    let transactionHeader = ['DEPOSIT', 'WITHDRAW', 'TRANSFER'];
+    if(transactionHeader.includes(pageHeaders.innerHTML)){
+        let currentAmountChecking = document.getElementById("current-amount-checking");
+        let currentAmountSaving = document.getElementById("current-amount-saving");
+        console.log(currentAmountChecking);
+        console.log(currentAmountSaving)
+        currentAmountSaving.style.display = "none";
+        accountType.addEventListener("change", e => {
+            if(e.target.value == 'Saving'){
+                currentAmountChecking.style.display = "none";
+                currentAmountSaving.style.display = "block";
+            }else{
+                currentAmountChecking.style.display = "block";
+                currentAmountSaving.style.display = "none";
+            }
+        }) 
+    }
 }
-
-// function amountMinMax(accountType){
-//     let transactionType = document.getElementById("transactionType").innerHTML;
-//     let currentAmountChecking = document.getElementById("current-amount-checking");
-//     let currentAmountSaving = document.getElementById("current-amount-saving");
-//     let checkingAmount = parseInt(currentAmountChecking.innerHTML.split("$")[1]);
-//     let savingAmount = parseInt(currentAmountSaving.innerHTML.split("$")[1]);
-//     let checkingDisplay = currentAmountChecking.style.display;
-//     let savingDisplay = currentAmountSaving.style.display;
-//     console.log(checkingAmount);
-//     accountType.addEventListener('input', () => {
-//         let v = parseInt(this.value);
-//         if(transactionType == 'DEPOSIT'){
-//             if (v > 2000) this.value = 2000;
-//         }else{
-//             if(checkingDisplay === 'block'){
-//                 if(v > checkingAmount) this.value = checkingAmount;
-//             }else if(savingDisplay === 'block'){
-//                 if(v > savingAmount) this.value = savingAmount;
-//             }
-//         }
-//     })
-// }
